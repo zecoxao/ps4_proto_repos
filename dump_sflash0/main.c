7e78fa97ac6f4481eae503b00203d4f6c5e69ad5
Your Name <you@example.com>
2024-01-20 16:01:04 +0000

    add dump sflash0

---
+++ dump_sflash0/main.c
@@ -0,0 +1,189 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <kernel.h>
+#include <libdbg.h>
+#include <sceerror.h>
+#include <scetypes.h>
+#include <net.h>
+#include <libhttp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/cdefs.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+
+struct filedesc {
+	void *useless1[3];
+	void *fd_rdir;
+	void *fd_jdir;
+};
+
+struct proc {
+	char useless[64];
+	struct ucred *p_ucred;
+	struct filedesc *p_fd;
+};
+
+struct thread {
+	void *useless;
+	struct proc *td_proc;
+};
+
+struct auditinfo_addr {
+	char useless[184];
+};
+
+struct ucred {
+	uint32_t useless1;
+	uint32_t cr_uid;     // effective user id
+	uint32_t cr_ruid;    // real user id
+	uint32_t useless2;
+	uint32_t useless3;
+	uint32_t cr_rgid;    // real group id
+	uint32_t useless4;
+	void *useless5;
+	void *useless6;
+	void *cr_prison;     // jail(2)
+	void *useless7;
+	uint32_t useless8;
+	void *useless9[2];
+	void *useless10;
+	struct auditinfo_addr useless11;
+	uint32_t *cr_groups; // groups
+	uint32_t useless12;
+};
+
+uint64_t __readmsr(unsigned long __register)
+{
+	unsigned long __edx;
+	unsigned long __eax;
+	__asm__("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(__register));
+	return (((uint64_t)__edx) << 32) | (uint64_t)__eax;
+}
+
+#define X86_CR0_WP (1 << 16)
+uint64_t cr0;
+
+static inline __attribute__((always_inline)) uint64_t readCr0(void)
+{
+	__asm__ volatile ("movq %0, %%cr0" : "=r" (cr0) : : "memory");
+	return cr0;
+}
+
+static inline __attribute__((always_inline)) void writeCr0(uint64_t cr0)
+{
+	__asm__ volatile("movq %%cr0, %0" : : "r" (cr0) : "memory");
+}
+
+int khax(struct thread* td, uint64_t* uap) {
+    size_t(*kprintf)(const char* fmt, ...) = (void*)0xFFFFFFFF824CE1A0ULL;
+    kprintf("entering kthread\n");
+
+    struct ucred* cred;
+    struct filedesc* fd;
+
+    fd = td->td_proc->p_fd;
+    cred = td->td_proc->p_ucred;
+
+
+    uint8_t* kernel_ptr = (uint8_t*)0xFFFFFFFF82200000;
+    void** got_prison0 = (void**)0xFFFFFFFF82C58BF0;
+    void** got_rootvnode = (void**)0xFFFFFFFF82FF8710;
+
+    cred->cr_uid = 0;
+    cred->cr_ruid = 0;
+    cred->cr_rgid = 0;
+    cred->cr_groups[0] = 0;
+
+    cred->cr_prison = *got_prison0;
+    fd->fd_rdir = fd->fd_jdir = *got_rootvnode;
+
+    // escalate ucred privs, needed for access to the filesystem ie* mounting & decrypting files
+    void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
+
+                                                     // sceSblACMgrIsSystemUcred
+    uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+    *sonyCred = 0xffffffffffffffff;
+
+    // sceSblACMgrGetDeviceAccessType
+    uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
+    *sceProcType = 0x3801000000000013; // Max access
+
+                                       // sceSblACMgrHasSceProcessCapability
+    uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+    *sceProcCap = 0xffffffffffffffff; // Sce Process
+
+
+    
+
+    kprintf("return to userland\n");
+
+    return 0;
+}
+
+/* Main entry point of program*/
+SceInt32 main(int argc, const char *const argv[])
+{
+	
+
+	printf("getuid:%08X\n", getuid());
+
+	printf("getpid:%08X\n", getpid());
+	
+	syscall(11, khax);
+	
+	printf("rechecking getuid:%08X\n", getuid());
+
+	printf("rechecking getpid:%08X\n", getpid());
+	
+	if(getuid()==0){
+		printf("Successfully jailbroken!\n");
+	}
+	
+	ssize_t nrd; 
+    int fd; 
+    int fd1; 
+
+    char buffer[0x200]; 
+
+    fd = open("/dev/sflash0", O_RDONLY, 0);
+    printf("open %08X\n",fd);
+    fd1 = open("/data/sflash0",  O_WRONLY | O_CREAT | O_TRUNC,  0777);
+    printf("open1 %08X\n",fd1);
+    
+	//0x10000
+	
+	int i=0;
+	
+	for(i=0;i<0x10000;i++){
+		if(i%0x1000 == 0){
+			printf("%08X\n",i);
+		}
+		nrd = read(fd,buffer,0x200);
+		if(nrd<0){
+			printf("read error:%s\n",strerror(errno));
+		}
+		int written = write(fd1,buffer,nrd);
+		if(written<0){
+			printf("write error:%s\n",strerror(errno));
+		}
+	}
+   
+    close(fd); 
+    printf("close\n");
+    close(fd1);
+    printf("close1\n");
+
+	
+	return 0;
+}
